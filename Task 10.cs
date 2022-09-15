86ef64f043419164f2327b03d2649aa7a64003e6
Вергилий Двачевский <Vergils2ch@gmail.com>
2022-09-15 15:44:31 +0300

    сделал 1 задание из 3

---
+++ Task 10.cs
@@ -0,0 +1,13 @@
+﻿
+//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+
+Console.Write("введите трехъзначное число");
+int x=Convert.ToInt32(Console.ReadLine());
+if   (x<=99)
+Console.Write("Отсутствует 3 я цифра   ");
+else if (x>=1000)
+Console.WriteLine("вы ввели слишком большое число");
+else 
+ Console.WriteLine(Convert.ToString(x)[1]);
+
